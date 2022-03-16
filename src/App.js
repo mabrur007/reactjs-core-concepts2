c47b47286b1da97d3aa3d0a0ce084e2a579496bf
mabrur007 <mirmd.mabrur@gmail.com>
2022-03-16 15:58:28 +0600

    dynamic data inserted through map

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const products = [
+    {name: 'laptop', price: 60000},
+    {name: 'mobile', price: 40000},
+    {name: 'watch', price: 6000},
+    {name: 'shoe', price: 2000},
+  ];
   return (
     <div className="App">
-      <Product name="iPhone 12pro max" price="20000"></Product>
+      {
+        products.map(product => <Product name={product.name} price={product.price}></Product>)
+      }
+      {/* <Product name="iPhone 12pro max" price="20000"></Product>
       <Product name="Samsung 12" price="30000"></Product>
-      <Product name="shaomi note 55" price="10000"></Product>
+      <Product name="shaomi note 55" price="10000"></Product> */}
     </div>
   );
 }
