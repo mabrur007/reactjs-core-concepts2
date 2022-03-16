2c2a29e26c62798c2772ca9f781f1a4ea1e91af8
mabrur007 <mirmd.mabrur@gmail.com>
2022-03-16 17:09:35 +0600

    new component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  const products = [
+  return (
+    <div className="App">
+      {/* <Counter></Counter>  */}
+      <ExternalUsers></ExternalUsers>
+    </div>
+  );
+}
+// my components 
+
+function ExternalUsers() {
+  return (
+    <div>
+      <h2>External Users</h2>
+    </div>
+  )
+}
+
+// another one
+function Counter() {
+  const [count, setCount] = useState(30);
+
+  const increaseCount = () => setCount(count + 1); 
+  const decreaseCount = () => setCount(count - 1);
+  
+  return (
+    <div>
+      <h1>Counter: {count} </h1>
+      <button onClick={increaseCount}>Increse</button>
+      <button onClick={decreaseCount}>Decrease</button>
+    </div>
+  );
+}
+
+
+/* 
+const products = [
     {name: 'laptop', price: 60000},
     {name: 'mobile', price: 40000},
     {name: 'watch', price: 6000},
     {name: 'shoe', price: 2000},
   ];
-  return (
-    <div className="App">
-      {
-        products.map(product => <Product name={product.name} price={product.price}></Product>)
-      }
-      {/* <Product name="iPhone 12pro max" price="20000"></Product>
-      <Product name="Samsung 12" price="30000"></Product>
-      <Product name="shaomi note 55" price="10000"></Product> */}
-    </div>
-  );
-}
+*/
 
-// my components 
+/* 
+  {
+    products.map(product => <Product name={product.name} price={product.price}></Product>)
+  }
+  <Product name="iPhone 12pro max" price="20000"></Product>
+  <Product name="Samsung 12" price="30000"></Product>
+  <Product name="shaomi note 55" price="10000"></Product>
+*/
+
+/* 
 function Product(props) {
   return (
     <div className='product'>
@@ -28,6 +63,7 @@ function Product(props) {
       <h4>Price: { props.price}</h4>
     </div>
   );
-}
+} 
+*/
 
 export default App;
