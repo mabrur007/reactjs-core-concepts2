e9e53212c888cc236e08c9355b93cdeaeff4c7a9
mabrur007 <mirmd.mabrur@gmail.com>
2022-03-16 17:41:25 +0600

    user added through json placeholder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
@@ -13,9 +13,30 @@ function App() {
 // my components 
 
 function ExternalUsers() {
+  const [users, setUsers] = useState([]);
+  // useEffect(() => { }, []) 
+  // const myFunc = () => {}
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUsers(data));
+  }, []);
   return (
     <div>
       <h2>External Users</h2>
+      <p>{users.length}</p>
+      {
+        users.map(user => <User name={user.name} email={user.email}></User>)
+      }
+    </div>
+  )
+}
+
+function User(props) {
+  return (
+    <div className='user'>
+      <h3>Name: {props.name} </h3>
+      <p>Email: {props.email}</p>
     </div>
   )
 }
