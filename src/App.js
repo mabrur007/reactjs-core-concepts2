b6f0a594dc6feb1b41b4d59fab2d639cbed65b8c
mabrur007 <mirmd.mabrur@gmail.com>
2022-03-16 15:41:10 +0600

    first component design

--- src/App.js
+++ src/App.js
@@ -4,20 +4,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Product></Product>
+      <Product></Product>
+      <Product></Product>
+    </div>
+  );
+}
+
+// my components 
+function Product() {
+  return (
+    <div className='product'>
+      <h2>Product: </h2>
+      <h4>Price: </h4>
     </div>
   );
 }
